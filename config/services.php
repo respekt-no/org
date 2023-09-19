9ed59e3246925b9b67df25814e3bfa498eb7a851
Leo Valen <leo@valen.pro>
2023-09-19 09:30:25 +0200

    initial development

--- config/services.php
+++ config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'vipps' => [
+        'client_id' => env('VIPPS_CLIENT_ID'),
+        'client_secret' => env('VIPPS_CLIENT_SECRET'),
+        'redirect' => env('VIPPS_REDIRECT_URI'),
+    ],
+
 ];
