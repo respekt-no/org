5d0abbe5b1487cd63e1fe503ba3d6b9165e75f51
Leo Valen <leo@valen.pro>
2023-09-21 23:10:33 +0200

    initial development

---
+++ database/migrations/2023_09_21_202031_create_memberships_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->foreignId('organization_id');
+            $table->dateTime('valid_from')->nullable()->index();
+            $table->dateTime('valid_to')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
